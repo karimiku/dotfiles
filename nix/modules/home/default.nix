1d621d6f6ec5d39a567723adb41c94860d985df6
kamiriku <kamicity.manriku@icloud.com>
2026-05-11 16:44:42 +0900

    hm: manage all dotfiles via home-manager (ghostty/tmux/nvim/vim/zsh/git)

--- nix/modules/home/default.nix
+++ nix/modules/home/default.nix
@@ -1,11 +1,32 @@
 { config, pkgs, ... }:
 
+let
+  # ~/dotfiles 直下の生ファイルへ直接 symlink を貼るヘルパー。
+  # /nix/store にコピーしないので、編集したら即反映（rebuild不要）。
+  dot = path: config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/${path}";
+in
 {
   home.username = "kamiriku";
   home.homeDirectory = "/Users/kamiriku";
   home.stateVersion = "24.05";
 
-  # 最小ゴール: starship.toml を home-manager 管理化
-  # → switch すると ~/.config/starship.toml が /nix/store への symlink になる
-  xdg.configFile."starship.toml".source = ../../../starship/dot-config/starship.toml;
+  # ~/.config/ 配下
+  xdg.configFile = {
+    "starship.toml".source = dot "starship/dot-config/starship.toml";
+    "ghostty".source       = dot "ghostty/dot-config/ghostty";
+    "nvim".source          = dot "nvim/dot-config/nvim";
+    "git/ignore".source    = dot "git/dot-config/git/ignore";
+  };
+
+  # ~/ 直下のドットファイル
+  home.file = {
+    ".tmux.conf".source = dot "tmux/dot-tmux.conf";
+    ".vimrc".source     = dot "vim/dot-vimrc";
+    ".zshrc".source     = dot "zsh/dot-zshrc";
+    ".zshenv".source    = dot "zsh/dot-zshenv";
+    ".zprofile".source  = dot "zsh/dot-zprofile";
+    ".p10k.zsh".source  = dot "zsh/dot-p10k.zsh";
+    ".gitconfig".source = dot "git/dot-gitconfig";
+    ".gitignore".source = dot "git/dot-gitignore";
+  };
 }
